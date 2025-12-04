4e7cba3cfaa13886bbe4b7078b017065690d6531
Jere Sotaniemi <jere.sotaniemi@gmail.com>
2025-12-04 17:53:04 +0200

    simulate app breaking down

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('7') // change this string to ensure a new version deployed
+  res.send('8') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
   res.send('ok')
+})
+
+// Simulate application breaking down
+app.get('/', (req, res) => {
+  res.status(500).send('Broken on purpose')
 })
 
 app.listen(PORT, () => {
